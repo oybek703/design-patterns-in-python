221a76b740aad1857c0e70cb933b94853e4a3777
oybek703 <hhoybek@gmail.com>
2022-02-15 15:37:13 +0500

    open closed principle

--- main.py
+++ main.py
@@ -1,31 +1,87 @@
-class Journal:
-    def __init__(self):
-        self.entries = []
-        self.count = 0
-
-    def add_entry(self, text):
-        self.count += 1
-        self.entries.append(f'{self.count}: {text}')
-
-    def remove_entry(self, index):
-        del self.entries[index]
-
-    def __str__(self):
-        return '\n'.join(self.entries)
+from enum import Enum
 
 
-class PersistentManager:
-    @staticmethod
-    def save_to_file(filename, journal):
-        file = open(filename, 'w')
-        file.write(str(journal))
-        file.close()
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
 
 
-journal = Journal()
-journal.add_entry('Play football.')
-journal.add_entry('Ate apple pie.')
+class Size(Enum):
+    SMALL = 1
+    MEDIUM = 2
+    LARGE = 3
 
-PersistentManager.save_to_file('./test.txt', journal)
-with open('./test.txt') as fh:
-    print(fh.read())
+
+class Product:
+    def __init__(self, name, color, size):
+        self.name = name
+        self.color = color
+        self.size = size
+
+
+class Specification:
+    def is_satisfied(self, spec):
+        pass
+
+    def __and__(self, other):
+        return AndSpecification(self, other)
+
+
+class Filter:
+    def filter(self, items, spec):
+        pass
+
+
+class ColorSpecification(Specification):
+    def __init__(self, color):
+        self.color = color
+
+    def is_satisfied(self, item):
+        return item.color == self.color
+
+
+class SizeSpecification(Specification):
+    def __init__(self, size):
+        self.size = size
+
+    def is_satisfied(self, item):
+        return item.size == self.size
+
+
+class AndSpecification(Specification):
+    def __init__(self, *args):
+        self.args = args
+
+    def is_satisfied(self, item):
+        return all(map(lambda spec: spec.is_satisfied(item), self.args))
+
+
+class BetterFilter(Filter):
+    def filter(self, items, spec):
+        for item in items:
+            if spec.is_satisfied(item):
+                yield item
+
+
+apple = Product('Apple', Color.GREEN, Size.SMALL)
+tree = Product('Tree', Color.GREEN, Size.LARGE)
+house = Product('House', Color.BLUE, Size.LARGE)
+products = [apple, tree, house]
+bf = BetterFilter()
+green = ColorSpecification(Color.GREEN)
+large = SizeSpecification(Size.LARGE)
+print('Green products: ')
+for p in bf.filter(products, green):
+    print(f' - {p.name} is green.')
+
+print('Large products: ')
+for p in bf.filter(products, large):
+    print(f' - {p.name} is large.')
+
+# large_blue = AndSpecification(large, ColorSpecification(Color.BLUE))
+large_blue = large & ColorSpecification(Color.BLUE)
+
+print('Large and blue products: ')
+for p in bf.filter(products, large_blue):
+    print(f' - {p.name} is large and blue.')
