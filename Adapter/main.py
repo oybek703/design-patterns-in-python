9639172cee0c26264ab845fbfa90e302ac2dce0a
oybek703 <hhoybek@gmail.com>
2022-02-17 18:52:33 +0500

    adapter pattern reviewed

---
+++ Adapter/main.py
@@ -0,0 +1,44 @@
+class Target:
+    """
+    The Target defines the domain-specific interface used by the client code.
+    """
+
+    def request(self) -> str:
+        return "Target: The default target's behavior."
+
+
+class Adaptee:
+    """
+    The Adaptee contains some useful behavior, but its interface is incompatible
+    with the existing client code. The Adaptee needs some adaptation before the
+    client code can use it.
+    """
+
+    def specific_request(self) -> str:
+        return ".eetpadA eht fo roivaheb laicepS"
+
+
+class Adapter(Target, Adaptee):
+
+    def request(self) -> str:
+        return f'Adapter: (TRANSLATED) {self.specific_request()[::-1]}'
+
+
+def client_code(target: 'Target') -> None:
+    print(target.request(), end=' ')
+
+
+print("Client: I can work just fine with the Target objects:")
+target = Target()
+client_code(target)
+print('\n')
+
+adaptee = Adaptee()
+print("Client: The Adaptee class has a weird interface. "
+      "See, I don't understand it:")
+print(adaptee.specific_request())
+print('\n')
+
+print("Client: But I can work with it via the Adapter:")
+adapter = Adapter()
+client_code(adapter)
